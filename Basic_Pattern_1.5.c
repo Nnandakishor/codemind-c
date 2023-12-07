e5b8e4cf32744e0c6f914dd4cf2adc891e8b79b5
Nnandakishor <147476605+Nnandakishor@users.noreply.github.com>
2023-12-07 20:39:32 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=65;i<=64+n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_1.5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=65;i<=64+n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%c ",i);
+        }
+        printf("
+");
+    }
+}
